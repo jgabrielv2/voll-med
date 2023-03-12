12e908827b36c393298853d9fc923c6c735d3258
jgabrielv <jgabriel.vidal@outlook.com>
2023-03-12 19:59:30 -0300

    configurado controle de acesso

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class TokenService {
 
     @Value("${api.security.token.secret}")
     private String secret;
+
     public String gerarToken(Usuario u) {
         try {
             var algoritmo = Algorithm.HMAC256(secret);
@@ -35,6 +37,23 @@ public class TokenService {
 
     private Instant dataExpiracao() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+
+            return JWT.require(algoritmo)
+                    // specify an specific claim validations
+                    .withIssuer("API Voll.med")
+                    // reusable verifier instance
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+
+        } catch (JWTVerificationException ex) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
     }
 
 }
